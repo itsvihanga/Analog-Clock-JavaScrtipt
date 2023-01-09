64d0bcaf0dddc53258f8bcc70e9829c3e64b387b
itsvihanga <IT21251078@my.sliit.lk>
2023-01-09 16:43:23 +0530

    change SJ and HTML recheck

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-setInterval(setclock , 1000)
+setInterval(setclock, 1000)
 
 const hourhand = document.querySelector(['hour-hand'])
 const minutshand = document.querySelector(['minuts-hand'])
 const secondhand = document.querySelector(['second-hand'])
 
-function setclock(){
+function setclock() {
     const currentDate = new Date();
-    const second = currentDate.getSeconds()/60
-    const minutes = (second + currentDate.getMinutes()) / 60 
-    const hours = (minutes + currentDate.getHours()) /12
-    setRotation(secondhand,second)
-    setRotation(minutshand,minutes)
-    setRotation(hourhand,hours)
+    const secondratio = currentDate.getSeconds() / 60
+    const minutesratio = (secondratio + currentDate.getMinutes()) / 60
+    const hoursratio = (minutesratio + currentDate.getHours()) / 12
+    setRotation(secondhand, secondratio)
+    setRotation(minutshand, minutesratio)
+    setRotation(hourhand, hoursratio)
 }
 
-function setRotation(element,rotationRatio){
+function setRotation(element, rotationRatio) {
     element.style.setProperty('--rotation', rotationRatio * 360)
 }
 setclock()
+
+
